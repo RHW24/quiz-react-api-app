3a577fead30974b6e03140ba96f7a416ff9903a2
RHW24 <ryanhartono2409@com>
2024-01-16 15:11:37 +0700

    first commit all file ready to use using vite and npm install

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { CreateQuiz, Loader, Quiz, Results } from "components";
+import { useGlobalProvider } from "hooks";
+import { useMemo } from "react";
+import { View } from "types";
+
+
+
+function App() {
+ 
+  const {showLoader, view} = useGlobalProvider();
+  const renderView = useMemo (() => { 
+    switch (view) {
+      case View.Quiz:
+        return <Quiz/>;
+      case View.Done:
+        return <Results/>;
+      default:
+        return <CreateQuiz />;
+    }
+  },[view]);
+
+  return (
+    <>
+      <div className="container is-max-desktop py-4">
+        <h1 className="title has-text-centered">
+          React Quiz with Open Trivia Database API
+        </h1>
+        {renderView}
+      </div>
+       {showLoader && <Loader/>}
+    </>
+  );
+}
+
+export default App
